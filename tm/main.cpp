fa7793f1ce77103cf9e79b49187b4a37e6a4d4e0
Lansdon Page <lansdon.page@email.wsu.edu>
2013-03-25 16:26:48 -0700

    Initial Commit

---
+++ tm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tm
+//
+//  Created by Lansdon Page on 3/25/13.
+//  Copyright (c) 2013 Lansdon Page. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
